03c4ca1e130d6b65eb37981bdd8ea89e57b3aa52
Kaela Dothard <92468593+kae-dot@users.noreply.github.com>
2023-03-16 09:09:02 -0400

    signup app working

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
+//jshint esversion:6
 const express = require('express');
-
 const bodyParser = require('body-parser');
-
 const request = require('request');
 
 const https = require('https');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/signup.html');
@@ -19,6 +18,8 @@ app.post('/', (req, res) => {
 	const firstName = req.body.firstName;
 	const lastName = req.body.lastName;
 	const email = req.body.email;
+
+	console.log(firstName, lastName, email);
 
 	var data = {
 		members: [
@@ -32,7 +33,7 @@ app.post('/', (req, res) => {
 			},
 		],
 	};
-	//console.log(firstName, lastName, email);
+
 	const jsonData = JSON.stringify(data);
 	const url = 'https://us21.api.mailchimp.com/3.0/lists/a753f08fd7';
 	var options = {
@@ -47,11 +48,11 @@ app.post('/', (req, res) => {
 			res.sendFile(__dirname + '/failure.html');
 		}
 
-		response.on('data', (data) => {
-			console.log(JSON.parse(data));
-		});
+		//response.on('data', (data) => {
+		//console.log(JSON.parse(jsonData));
+		//});
 	});
-	//request.write(jsonData);
+	request.write(jsonData);
 	request.end();
 });
 
